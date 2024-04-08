6cf12e862cdc74a28a821e9edaed5e7412f7bd96
Karov4 <dimitarkarov18v@gmail.com>
2024-04-08 18:48:26 +0300

    change admin

--- apartments/admin.py
+++ apartments/admin.py
@@ -3,34 +3,38 @@ from .models import Apartment, Rent, Review, Favorite
 
 
 class ApartmentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price', 'available', 'owner', 'location')
-    list_filter = ('available', 'owner')
-    search_fields = ('name', 'description', 'location')
-    ordering = ('price',)
+    list_display = ('name', 'price', 'available', 'location')
+    list_filter = ('available',)
+    search_fields = ('name', 'location')
+    ordering = ('name',)
+    readonly_fields = ('owner',)
 
 
-class LeaseAdmin(admin.ModelAdmin):
+class RentAdmin(admin.ModelAdmin):
     list_display = ('tenant', 'apartment', 'start_date', 'end_date')
-    list_filter = ('tenant', 'apartment')
+    list_filter = ('start_date', 'end_date')
     search_fields = ('tenant__username', 'apartment__name')
-    ordering = ('start_date',)
+    ordering = ('-start_date',)
+    readonly_fields = ('tenant', 'apartment')
 
 
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('rating', 'comment', 'lease', 'date')
-    list_filter = ('rating', 'lease')
-    search_fields = ('comment',)
-    ordering = ('date',)
+    list_display = ('lease', 'rating', 'date')
+    list_filter = ('rating', 'date')
+    search_fields = ('lease__tenant__username', 'lease__apartment__name')
+    ordering = ('-date',)
+    readonly_fields = ('lease',)
 
 
 class FavoriteAdmin(admin.ModelAdmin):
-    list_display = ('apartment', 'user')
-    list_filter = ('apartment', 'user')
-    search_fields = ('apartment__name', 'user__username')
+    list_display = ('user', 'apartment')
+    list_filter = ('user',)
+    search_fields = ('user__username', 'apartment__name')
     ordering = ('user',)
+    readonly_fields = ('user', 'apartment')
 
 
 admin.site.register(Apartment, ApartmentAdmin)
-admin.site.register(Rent, LeaseAdmin)
+admin.site.register(Rent, RentAdmin)
 admin.site.register(Review, ReviewAdmin)
 admin.site.register(Favorite, FavoriteAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'user_type', 'age')
-    list_filter = ('user_type',)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'age', 'user_type', 'is_staff')
+    list_filter = ('user_type', 'is_staff')
     search_fields = ('username', 'email')
     ordering = ('username',)
+    readonly_fields = ('last_login', 'date_joined')
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
